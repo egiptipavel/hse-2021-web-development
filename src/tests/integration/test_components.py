dae0d29bfc0c5a40ee49f2b8f8d5b83092d8dc6e
egiptipavel <kinqstone585@gmail.com>
2021-09-26 15:42:12 +0300

    Change integration tests for components and orders

--- src/tests/integration/test_components.py
+++ src/tests/integration/test_components.py
@@ -1,18 +1,10 @@
-from fastapi.encoders import jsonable_encoder
-from fastapi.testclient import TestClient
-
-from src.endpoints.components import components
-from main import app
+from src.endpoints.components import components, add
 from src.models.component import Component
-from src.models.response import Response
-
-client = TestClient(app)
 
 
 def test_add_component():
     components.clear()
     component = Component(id=1, name="name", type="type", cost=1)
-    body_of_response = Response(response="Component added successfully")
-    response = client.post("/components/", json=jsonable_encoder(component))
-    assert response.status_code == 200
-    assert response.json() == body_of_response
+    result = add(component)
+    assert result.get("status_code") == 200
+    assert result.get("content").get("response") == "Component added successfully"
--- src/tests/integration/test_orders.py
+++ src/tests/integration/test_orders.py
@@ -13,18 +13,18 @@ def test_create_order_successful():
     assert result.get("content").get("response") == "Order created"
 
 
-# def test_create_order_not_successful():
-#     orders.clear()
-#     count_of_orders.pop()
-#     count_of_orders.append(1)
-#     response = client.post("/orders/?id_user=1", json=[1])
-#     assert response.status_code == 400
-#     assert response.json() == {"error": "No component with 1 id"}
-#
-#
-# def test_create_order_twice():
-#     test_create_order_successful()
-#     test_add_component()
-#     response = client.post("/orders/?id_user=1", json=[1])
-#     assert response.status_code == 200
-#     assert response.json() == {"response": "Order created"}
+def test_create_order_not_successful():
+    orders.clear()
+    count_of_orders.pop()
+    count_of_orders.append(1)
+    result = create(1, [1])
+    assert result.get("status_code") == 400
+    assert result.get("content").get("error") == "No component with 1 id"
+
+
+def test_create_order_twice():
+    test_create_order_successful()
+    test_add_component()
+    result = create(1, [1])
+    assert result.get("status_code") == 200
+    assert result.get("content").get("response") == "Order created"
